0a2e3e6d51262cc628b26675aab7c795058fd19f
mthakkar011 <60222439+mthakkar011@users.noreply.github.com>
2020-08-11 10:22:42 +0530

    Initial Commit

---
+++ Readme.md
@@ -0,0 +1,53 @@
+# HTMLX Pre-Processor
+
+When designing complex webpages, it becomes important to reuse components and make the pages modular.  For e.g. instead of repeating same footer code on all webpages, it makes more sense to write a footer.html file and include it in all your webpages.
+
+On server-side most web frameworks support this using {{include}} statements; however this is not easily available on client side.  
+
+HTMLX python based pre-processor that allows you to write HTML files and use dynamic statements like ##include footer.html Which then generates html files in the dist folder without any JS dependencies. 
+
+## Install Dependencies
+
+```bash
+pip install watchdog
+```
+
+## Usage
+Download the HTMLX.py and place it in your project folder 
+```
+project_folder
+│   htmlx.py
+|───static
+|   |   
+│   |   index.htmlx
+|   |   about.htmlx
+|   |   footer.html
+│
+└───dist
+│   │   index.html
+│   │   about.html
+```
+
+1. Create  a folder called static. Inside this folder create files called index.htmlx   about.htmlx  footer.html
+2. Open htmlx files in any text editor, and inside body tags include footer.html using 
+`
+##include footer.html
+`
+
+```bash
+cd project_folder
+python htmlx.py -c static/  -m prod
+```
+
+## Options
+Arguments
+
+-m (Mode):  Debug or Prod mode. In prod mode, it strips out all console.log statements
+
+-w <folder>: Watch folder for changes to all .htmlx files and automatically compile to generate results in dist folder
+
+-c <folder>: Compile the folder and generate files in dist
+
+
+## Known Issues
+Currently works only with Python2
---
+++ htmlx.py
@@ -0,0 +1,152 @@
+import sys,os
+import json,re
+import time
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+class Watcher:
+
+    def __init__(self,path):
+        self.observer = Observer()
+        self.path = path
+
+    def run(self):
+        event_handler = Handler()
+        self.observer.schedule(event_handler, self.path, recursive=True)
+        self.observer.start()
+        try:
+            while True:
+                time.sleep(10)
+        except:
+            self.observer.stop()
+            print("Error")
+
+        self.observer.join()
+
+
+class Handler(FileSystemEventHandler):
+   
+    @staticmethod
+    def on_any_event(event):
+        if event.is_directory:
+            return None
+
+        elif event.event_type == 'created':
+            # Take any action here when a file is first created.
+            print("Received created event - %s." % event.src_path)
+
+        elif event.event_type == 'modified':
+            # Taken any action here when a file is modified.    
+            path, fname = os.path.split(event.src_path)
+            if "dist" not in path:
+                print("Received modified event - %s." % event.src_path)
+                complileDir(path,mode="debug")
+
+def strip_lines(content):
+    exclude_list = [r"console\.log\(.*\)"]
+    m = re.match("|".join(exclude_list),content)
+    if m:
+        print("Found expression in exclude_list",m.group(0))
+        content = content.replace(m.group(0),"") ##Replace matched with empty string
+        
+    return content
+
+def compileHTML(directory,filename,mode):
+    try:
+        fin = os.path.join(directory, filename) 
+        content = open(fin).readlines()
+    except:
+        return "Cannot Read Input File",directory,filename     
+     
+    # Show the file contents line by line.
+    # We added the comma to print single newlines and not double newlines.
+    # This is because the lines contain the newline character '\n'. 
+    print("Got mode",mode)
+    for i in range(len(content)):
+        if mode == "prod":
+            content[i] = strip_lines(content[i])
+
+
+        if "##" not in content[i]: 
+            continue
+
+        print("found parser expression",content[i])
+        
+        # take everything after ## then split it by whitespace 
+        try:
+            expression = content[i].split("##")[1]
+            command = expression.split()[0]
+
+            loc = expression.split()[1]
+            if "(" in loc:
+                value = loc.split("(")[0]
+                args = loc.split("(")[1].replace(")",'').strip()
+                try:
+                    args = json.loads(args)
+                except:
+                    print("Unable to parse from params",args)
+                    raise Exception('Invalid params',expression)    
+            else:
+                value = loc
+                args = {}
+
+            print(command,value,args)
+        except:
+            print("Unidentified expression")
+            continue    
+        if command == "include":
+            try:
+                layout_file = os.path.join(directory, value) 
+                content[i] = open(layout_file).read()
+                if mode == "prod":
+                    content[i] = strip_lines(content[i])
+                    
+                for key,value in args.items():
+                    content[i] = content[i].replace("$"+key,value)
+                content[i] = content[i].encode('utf-8','ignore')  
+                  
+            except:
+                print("unable to include file: ",layout_file)     
+
+    content = ''.join(content)
+    try:     
+        fout = open(directory+"/dist/"+filename.replace(".htmlx",".html"),"w+")
+        fout.write(content)         
+    except:
+        print("unable to open file",directory+"/dist/"+filename.replace(".htmlx",".html"))            
+    
+    return "Sucess..."    
+
+
+def complileDir(directory,mode):    
+    for filename in os.listdir(directory):
+
+        if filename.endswith(".htmlx") or filename.endswith(".py"): 
+            print(filename)
+            print(compileHTML(directory,filename,mode))
+            # print()
+            continue
+        else:
+            continue
+
+
+if __name__=="__main__":
+    try:
+        if sys.argv[1] == "-w": 
+            w = Watcher(sys.argv[2])
+            w.run() 
+        else:
+            if sys.argv[4] == "prod":
+                mode = "prod"
+                complileDir(sys.argv[2],mode)
+            elif sys.argv[4] == "debug":
+                mode = "debug"
+                complileDir(sys.argv[2],mode)
+            else:
+                print("Unkown mode. use -m debug or prod",sys.argv[4] )
+
+            
+
+    except:
+        print("htmlx -w[Watch] -c[Complile] directory -m[prod | debug]")
+        
